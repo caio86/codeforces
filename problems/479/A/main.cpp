0a86e09ab05fb0e7525092f18e20b55c9474dfb5
caio86 <caioluiz86@gmail.com>
2024-11-17 19:35:06 -0300

    479A passou

---
+++ problems/479/A/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getMax(const int a, const int b,const int c) {
+  int o1 = a+b*c;
+  int o2 = a*(b+c);
+  int o3 = a*b*c;
+  int o4 = (a+b)*c;
+  int o5 = a+b+c;
+
+  int res = max(
+      max(o1, o2),
+      max(o3, o4)
+    );
+
+  return max(res, o5);
+}
+
+void solve() {
+  int a, b, c; cin >> a >> b >> c;
+
+  int max = getMax(a, b, c);
+
+  cout << max
+    << "\n";
+}
+
+int main() {
+  cin.tie(0); cout.tie(0);
+  ios::sync_with_stdio(0);
+  solve();
+  return 0;
+}
+
