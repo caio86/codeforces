89d6825f0f029b494415e2cb7a17b1188f4363d4
caio86 <caioluiz86@gmail.com>
2025-04-09 01:15:58 -0300

    contest 2093C passou

--- contest/2093/C/python.py
+++ contest/2093/C/python.py
@@ -1,25 +1,30 @@
 from math import sqrt
 
+
 def checkPrime(n):
     if n <= 1:
         return False
 
-    for i in range(2, int(sqrt(n))+1):
+    for i in range(2, int(sqrt(n)) + 1):
         if n % i == 0:
             return False
 
     return True
 
+
 T = int(input())
 
 for _ in range(T):
-    x, k =input().split()
+    x, k = input().split()
 
     x = int(x)
     k = int(k)
+
+    if k == 2 and x == 1:
+        print("YES")
+        continue
 
     if k == 1 and checkPrime(x):
         print("YES")
     else:
         print("NO")
-
